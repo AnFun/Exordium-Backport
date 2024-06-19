f6627a78370434866ddefe012839686a3e4eb323
quinn <qsatow@gmail.com>
2024-06-19 02:09:21 -0500

    what

--- src/main/java/dev/tr7zw/exordium/access/BossEventBufferAccess.java
+++
@@ -1,7 +0,0 @@
-package dev.tr7zw.exordium.access;
-
-import dev.tr7zw.exordium.util.BufferedComponent;
-
-public interface BossEventBufferAccess {
-    BufferedComponent exordium_getBuffered();
-}
--- src/main/java/dev/tr7zw/exordium/access/ChatAccess.java
+++
@@ -1,9 +0,0 @@
-package dev.tr7zw.exordium.access;
-
-import dev.tr7zw.exordium.access.VanillaBufferAccess.ChatOverlayAccess;
-
-public interface ChatAccess extends ChatOverlayAccess {
-
-    public void updateState(int tickCount);
-
-}
